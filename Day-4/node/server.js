a828edd6cd67a87d8a65d8e1a7fb331884bb1d0a
prateek959 <prateekkush8081@gmail.com>
2025-04-12 15:04:09 +0530

    day-4

--- Day-4/node/server.js
+++ Day-4/node/server.js
@@ -4,22 +4,28 @@ import morgan from 'morgan';
 
 const app = express();
 // app.use(morgan(':method :url :response-time'));
-if(process.env.NODE_ENV === "DEVELOPMENT"){
-    app.use((req,res,next)=>{
+if (process.env.NODE_ENV === "DEVELOPMENT") {
+    app.use((req, res, next) => {
         console.log(`Method is ${req.method} and URL is ${req.originalUrl}`);
         next();
     })
 };
 
-app.get('/get',(req, res)=>{
+app.get('/get', (req, res) => {
     res.send("Hello world");
-}); 
+});
 
-app.get('/debounce',(req,res)=>{
-    
-})
+let timeout;
+app.get('/debounce', (req, res) => {
+    const {search} = req.query;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+        console.log(search);
+        res.send(search);
+    }, 5000);
+});
 
 
-app.listen(3030,()=>{
+app.listen(3030, () => {
     console.log(`Server is running on 3030`);
 });
